1b7453436acbb6e620ff333481f98049dd1476fd
Niklas Haller <niklash2021@gmail.com>
2024-04-16 17:26:35 +0200

    Adding Rest Api in Kotlin

---
+++ InvoiceGeneratorRestAPI/src/main/kotlin/com/example/invoicegeneratorrestapi/InvoiceGeneratorRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.invoicegeneratorrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceGeneratorRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvoiceGeneratorRestApiApplication>(*args)
+}
---
+++ InvoiceGeneratorRestAPI/src/test/kotlin/com/example/invoicegeneratorrestapi/InvoiceGeneratorRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.invoicegeneratorrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoiceGeneratorRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
